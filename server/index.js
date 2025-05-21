9d0b6f35b252ffb9fc995531c9a97cbe83d85714
Sajal Laha <80203154+sajal327@users.noreply.github.com>
2025-05-22 02:44:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,12 +21,23 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  })
-);
+const corsOptions = {
+  origin: [
+    "process.env.FRONTEND_URL",
+    "http://localhost:5173",
+    "https://pabitra-bhandar-client.onrender.com", // your Render frontend URL
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.FRONTEND_URL,
+//   })
+// );
 
 app.use(express.json());
 app.use(cookieParser());
