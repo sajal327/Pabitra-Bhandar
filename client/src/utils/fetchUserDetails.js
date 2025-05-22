eeeb32ef20a8f16c5b5d77f940f485c0aee9c3fb
Sajal Laha <80203154+sajal327@users.noreply.github.com>
2025-05-22 15:21:10 +0530

    Update fetchUserDetails.js

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -1,15 +1,36 @@
-import Axios from "./Axios"
-import SummaryApi from "../common/SummaryApi"
+import Axios from "./Axios";
+import SummaryApi from "../common/SummaryApi";
 
-const fetchUserDetails = async()=>{
-    try {
-        const response = await Axios({
-            ...SummaryApi.userDetails
-        })
-        return response.data
-    } catch (error) {
-        console.log(error)
-    }
-}
+const fetchUserDetails = async () => {
+  try {
+    const response = await Axios({
+      ...SummaryApi.userDetails,
+    });
 
-export default fetchUserDetails
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user details:", error?.response?.data || error.message);
+    
+    // ❗️Throw the error so caller can handle it properly
+    throw error;
+  }
+};
+
+export default fetchUserDetails;
+
+
+// import Axios from "./Axios"
+// import SummaryApi from "../common/SummaryApi"
+
+// const fetchUserDetails = async()=>{
+//     try {
+//         const response = await Axios({
+//             ...SummaryApi.userDetails
+//         })
+//         return response.data
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// export default fetchUserDetails
