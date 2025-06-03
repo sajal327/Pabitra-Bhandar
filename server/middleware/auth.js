28dde044a52f9f821b86b6990c884eed890faa4f
Sajal Laha <80203154+sajal327@users.noreply.github.com>
2025-06-04 02:13:56 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,23 @@
-import jwt from "jsonwebtoken";
-const auth = async (req, res, next) => {
-  try {
-    const token = req.cookies.accessToken;
+// import jwt from "jsonwebtoken";
+// const auth = async (req, res, next) => {
+//   try {
+//     const token = req.cookies.accessToken;
 
-    if (!token) {
-      return res.status(401).json({ message: "Please login", success: false });
-    }
+//     if (!token) {
+//       return res.status(401).json({ message: "Please login", success: false });
+//     }
 
-    const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
-    req.userId = decoded.id;
+//     const decoded = jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+//     req.userId = decoded.id;
 
-    next();
-  } catch (err) {
-    res
-      .status(401)
-      .json({ message: "Unauthorized or invalid token", success: false });
-  }
-};
-export default auth;
+//     next();
+//   } catch (err) {
+//     res
+//       .status(401)
+//       .json({ message: "Unauthorized or invalid token", success: false });
+//   }
+// };
+// export default auth;
 
 // import jwt from "jsonwebtoken";
 
@@ -63,40 +63,40 @@ export default auth;
 
 // export default auth;
 
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-// const auth = async (request, response, next) => {
-//   try {
-//     const token =
-//       request.cookies.accessToken ||
-//       request?.headers?.authorization?.split(" ")[1];
+const auth = async (request, response, next) => {
+  try {
+    const token =
+      request.cookies.accessToken ||
+      request?.headers?.authorization?.split(" ")[1];
 
-//     if (!token) {
-//       return response.status(401).json({
-//         message: "Provide token",
-//       });
-//     }
+    if (!token) {
+      return response.status(401).json({
+        message: "Provide token",
+      });
+    }
 
-//     const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+    const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
 
-//     if (!decode) {
-//       return response.status(401).json({
-//         message: "unauthorized access",
-//         error: true,
-//         success: false,
-//       });
-//     }
+    if (!decode) {
+      return response.status(401).json({
+        message: "unauthorized access",
+        error: true,
+        success: false,
+      });
+    }
 
-//     request.userId = decode.id;
+    request.userId = decode.id;
 
-//     next();
-//   } catch (error) {
-//     return response.status(500).json({
-//       message: "You have not login", ///error.message || error,
-//       error: true,
-//       success: false,
-//     });
-//   }
-// };
+    next();
+  } catch (error) {
+    return response.status(500).json({
+      message: "You have not login", ///error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
 
-// export default auth;
+export default auth;
