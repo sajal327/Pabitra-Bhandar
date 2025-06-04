9d59eff99b6749867d0aa77f4b2589c42445daac
Sajal Laha <80203154+sajal327@users.noreply.github.com>
2025-06-04 13:17:30 +0530

    Update fetchUserDetails.js

--- client/src/utils/fetchUserDetails.js
+++ client/src/utils/fetchUserDetails.js
@@ -1,24 +1,46 @@
 // utils/fetchUserDetails.js
+
 import Axios from "./Axios";
 import SummaryApi from "../common/SummaryApi";
 
-const fetchUserDetails = async (token) => {
+const fetchUserDetails = async () => {
   try {
     const response = await Axios({
       ...SummaryApi.userDetails,
-      headers: {
-        Authorization: `Bearer ${token}`, // Attach token here
-      },
+      withCredentials: true,
     });
 
     return response.data;
   } catch (error) {
-    console.error("Error fetching user details:", error?.response?.data || error.message);
+    console.log("Error fetching user details:", error.response?.data || error);
     throw error;
   }
 };
 
 export default fetchUserDetails;
+
+
+//last 
+// import Axios from "./Axios";
+// import SummaryApi from "../common/SummaryApi";
+
+// const fetchUserDetails = async (token) => {
+//   try {
+//     const response = await Axios({
+//       ...SummaryApi.userDetails,
+//       headers: {
+//         Authorization: `Bearer ${token}`, // Attach token here
+//       },
+//     });
+
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching user details:", error?.response?.data || error.message);
+//     throw error;
+//   }
+// };
+
+// export default fetchUserDetails;
 
 
 // import Axios from "./Axios";
